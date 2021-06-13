f212a08c1b68eaecc70d929d5b79aefa6af2f2b4
Figen <tony_faye735@live.co.uk>
2021-06-13 15:22:44 +0100

    Debug pause on new script load

--- fileDecryption.py
+++ fileDecryption.py
@@ -1,2 +1,9 @@
+# Imports
+import os
+
+# Main
 def main():
+    os.system('cls')
+    
     print("FILE DECRYPTION LOADED SUCCESSFULLY")
+    os.system('pause')
--- fileEncryption.py
+++ fileEncryption.py
@@ -1,2 +1,9 @@
+# Imports
+import os
+
+# Main
 def main():
+    os.system('cls')
+    
     print("FILE ENCRYPTION LOADED SUCCESSFULLY")
+    os.system('pause')
